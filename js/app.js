3a77e523e0519e4711f4f19f714f07b38a10d9cb
Pobarul-islam <pobarul3@gmail.com>
2022-02-28 19:06:22 +0600

    meal db api project code added

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const searchBtn = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  // console.log(searchText);
+  searchField.value = "";
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+  // console.log(url)
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displaySearchResult(data.meals));
+};
+
+const displaySearchResult = (meals) => {
+  // console.log(meals)
+  const searchResult = document.getElementById("search-result");
+  meals.forEach((meal) => {
+    // console.log(meal);
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `  <div class="card">
+      <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5>${meal.strMeal}</h5>
+        <h5>Country: ${meal.strArea}</h5>
+       <p>${meal.strInstructions.slice(0, 250)}</p>
+
+      </div>
+    </div>`;
+     searchResult.appendChild(div);
+  });
+     
+};
