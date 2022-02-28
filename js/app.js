8ad30ac5adcebea3e436dcdd153726339860f03f
Pobarul-islam <pobarul3@gmail.com>
2022-02-28 19:51:24 +0600

    some code added

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ const displaySearchResult = (meals) => {
     // console.log(meal);
     const div = document.createElement("div");
     div.classList.add("col");
-    div.innerHTML = `  <div class="card">
+    div.innerHTML = `  <div onclick="loadMeailDetail(${meal.idMeal})" class="card">
       <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5>${meal.strMeal}</h5>
@@ -26,7 +26,28 @@ const displaySearchResult = (meals) => {
 
       </div>
     </div>`;
-     searchResult.appendChild(div);
+    searchResult.appendChild(div);
   });
-     
+};
+
+const loadMeailDetail = (meailid) => {
+  // console.log(meailid)
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meailid}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMealDetail(data.meals[0]));
+};
+
+const displayMealDetail = (meal) => {
+  console.log(meal);
+  const mealDetails = document.getElementById("meal-details");
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.innerHTML = ` <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+  <div class="card-body">
+   <h5>${meal.strMeal}</h5>
+   <p>${meal.strInstructions}</p>
+    <a href="${meal.strYoutube}" class="btn btn-primary">Youtube</a>
+  </div>`;
+       mealDetails.appendChild(div);
 };
